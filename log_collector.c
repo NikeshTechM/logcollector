21fa9e6739486ee6c79c9ef1d41dd8a9bf02e2a3
NikeshTechM <nikesh.sar@techmahindra.com>
2025-07-26 06:11:32 +0000

    Update log_collector.c

--- log_collector.c
+++ log_collector.c
@@ -49,7 +49,7 @@ int main() {
         log_disk_usage(f);
         fclose(f);
 
-        sleep(1);  // ✅ changed to 1 second-newone11
+        sleep(1);  // ✅ changed to 1 second-01
     }
     return 0;
 }
