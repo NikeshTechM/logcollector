319855338c8f349483e2d8a264d352f763cca4e1
NikeshTechM <nikesh.sar@techmahindra.com>
2025-08-11 19:59:17 +0530

    Update log_collector.c

--- log_collector.c
+++ log_collector.c
@@ -14,7 +14,7 @@ int main() {
     signal(SIGINT, handle_sigint);
 
     while (keep_running) {
-        printf("Hello from container v8.36\n");
+        printf("Hello from container v8.37\n");
         fflush(stdout);  // Flush output immediately
         sleep(3);
     }
